1e288272d87cff3db0ad8de6779abb4bd1b7f941
Connor <dunne064@umn.edu>
2015-10-25 22:22:59 -0500

    Timing mostly finished for polling aio

--- server.c
+++ server.c
@@ -26,7 +26,8 @@
 #include <sys/time.h>
 #include <aio.h>
 
-#define BUF_SIZE 1024 *2
+#define BUF_SIZE 1024
+#define AIO_LIST_SIZE 1024
 
 int will_aio_read;
 int port;
@@ -46,6 +47,11 @@ typedef struct {
 	struct timeval timer;
 	struct timeval orig_start;
 } threadarg;
+
+typedef struct time_tuple{
+    struct timeval start;
+    struct timeval end;
+} time_tuple;
 
 struct sockaddr_in server;
 
@@ -165,17 +171,16 @@ int thread_impl() {
 
 }
 
-int polling_impl() {
+int polling_impl_aio() {
 
     int clientsockfd;
+    int client_len;
     struct sockaddr_in client;
-    int threadno = 0;
-    fd_set fds;
-    int fd;
     struct aiocb aiocb;
-    struct aiocb emptyAiocb;    
     struct aiocb *aiocbp;
     int i = 0;
+    int total_finished = 0; //for calculating average time
+    suseconds_t total_time = 0.0; //for caluclating average time
     
     char buf2[BUF_SIZE];
 
@@ -184,9 +189,10 @@ int polling_impl() {
     server.sin_port = htons(port);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    fcntl(sockfd, F_SETFL, O_NONBLOCK); //Needs testing
+    //make socker non-blocking, for 'accept'
+    fcntl(sockfd, F_SETFL, O_NONBLOCK);
 
-    int client_len = sizeof(client); 
+    client_len = sizeof(client); 
 
     // Open socket
     if (sockfd < 0) 
@@ -203,109 +209,261 @@ int polling_impl() {
     // Listen on port
     listen(sockfd, 50);
 
-
-    //TODO: Make cleanup/addition to aio_list better (can add to old areas)
-
-
-    struct aiocb aio_list[100]; //TODO: Change in future
-    int completed[100];
-    for(i=0; i<100; i++){
-        completed[i] = 0;
-    }
+    //Setup aio array, time array, completion array
     int aio_count = 0;
-    int ret;
-
-
+    int ret, err;
+    
+    struct aiocb aio_list[AIO_LIST_SIZE];
+    struct time_tuple time_list[AIO_LIST_SIZE];
+    
+    int completed[AIO_LIST_SIZE];
+    for(i=0; i<AIO_LIST_SIZE; i++){
+        completed[i] = 0;
+        time_list[i].start.tv_sec = 0;
+        time_list[i].end.tv_sec = 0;
+    }
+    
     while(1) {
-        //sleep(1);
-        //usleep(100000);
-
-        //STAGE 1: Accept / Connect if possible
+    
+        /////////////////////////////////STAGE 1: Accept / Connect / Read if possible///////////////////////////////////////////////////
+        
         clientsockfd = accept(sockfd, (struct sockaddr *) &client, (socklen_t *) &client_len);
         
         if(clientsockfd < 0){
-            //printf("---Nothing here\n");
-            //nothing was there (or error)
+            //no new connections
         }
         else{
-            //TODO: Figure out where to write to buffers ... or if this works currently
+            //printf("Setting up new clientSockFd: %d\n", clientsockfd);
             
-            aiocb = emptyAiocb;
-            
-            printf("Setting up new clientSockFd: %d\n", clientsockfd);
-
-            //char *buffer = (char *)malloc(BUF_SIZE * sizeof(char));
-            
+            //Clear buff and struct
             memset(&buf2, 0, BUF_SIZE);
             memset(&aiocb, 0, sizeof(struct aiocb));
             aiocb.aio_fildes = clientsockfd;
             aiocb.aio_buf = buf2;
-            printf("### buf # %d equals %p\n", aio_count, buf2);
             aiocb.aio_nbytes = BUF_SIZE;
-            //NOTE: these args are basically the same as read(fd, buf, count)
 
-            //add to the array
+            //update arrays
             aio_list[aio_count] = aiocb;
-
-            //start download
-            if(aio_read(&aio_list[aio_count]) != 0) { printf("Error with aio_read\n"); }
+            gettimeofday(&(time_list[aio_count].start), NULL);
             
             aio_count++;
+
+            if (aio_count > AIO_LIST_SIZE){
+                printf("Too many connections for AIO_LIST_SIZE!");
+            }
+            
+            //start download
+            if(aio_read(&aio_list[aio_count-1]) != 0) {
+                printf("Error with aio_read\n");
+            }
+            
         }
 
-        //STAGE 2: Iterate through aiocb's, see if any have completed
+        /////////////////////////////STAGE 2: Iterate through aiocb's, see if any have completed//////////////////////////////////
+        
         for(i = 0; i < aio_count; i++){
-            aiocbp = &aio_list[i];
+            
             if (completed[i] == 1){
-                //already completed
+                //already completed read
                 continue;
             }
             
-            printf("Fildes: %d, aio_count: %d, i: %d\n", aiocbp->aio_fildes, aio_count, i);
-
-            int err = aio_error(aiocbp);
-
-
+            aiocbp = &aio_list[i];
+            err = aio_error(aiocbp);
+            
             if (err != 0 && err != EINPROGRESS) {
               printf ("Error at aio_error() : %s\n", strerror (err));
             }
             
             else if (err == EINPROGRESS){
                 //in progress, don't check return
-                printf("WIP\n");
             }
             
             else{
-                //no error, ready to return!
+                //no error, aio_read is finished
                 ret = aio_return(aiocbp);
                 
                 if (ret < 0) {
                   printf("Error at aio_return() : %s\n", strerror(errno));
-                  printf("##Ret val: %d, nbytes: %d\n", ret, aiocb.aio_nbytes);
-                  close(aiocbp->aio_fildes);
-                  exit(0); 
                 }
             
                 else if (ret > 0){
-                    printf("...Still reading, retval = %d\n", ret);
+                    //Partially finished; read next chunk into buffer
+                    //printf("...Still reading, retval = %d\n", ret);
                     aio_read(aiocbp);
                 }
 
                 else{
-                    //read successfil and finished
-
-                    //stop timer?
-                    //cleanup
-                    printf ("SUCCESS on %d\n", i);
-
-                    //free((void*)aiocb.aio_buf);
-                    close(aiocbp->aio_fildes);
-
-
+                    //Read totally finished
+                    //printf ("SUCCESS on %d\n", i);
+                                        
+                    //cleanup:
+                    close(aiocbp->aio_fildes);                    
                     completed[i] = 1;
+                    
+                    //timing:
+                    gettimeofday(&(time_list[i].end), NULL);
+                    total_finished++;
+                    
+                    suseconds_t startDouble = (time_list[i].start.tv_sec * 1000000 + time_list[i].start.tv_usec);
+                    suseconds_t endDouble = (time_list[i].end.tv_sec * 1000000 + time_list[i].end.tv_usec);
+                    suseconds_t timeDiff = endDouble - startDouble;
+                    
+                    total_time += timeDiff;
+                    
+                    printf("Client #%d took %ldusec to finish. New avg. time: %ldusec\n", i, timeDiff, total_time/total_finished);
+                    
                 }
             }
+        }
+    }
+    return 0;
+}
 
+int polling_impl_read() {
+
+    int clientsockfd;
+    int client_len;
+    struct sockaddr_in client;
+    struct aiocb aiocb;
+    struct aiocb *aiocbp;
+    int i = 0;
+    int total_finished = 0; //for calculating average time
+    suseconds_t total_time = 0.0; //for caluclating average time
+    
+    char buf2[BUF_SIZE];
+
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(port);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    //make socker non-blocking, for 'accept'
+    fcntl(sockfd, F_SETFL, O_NONBLOCK);
+
+    client_len = sizeof(client); 
+
+    // Open socket
+    if (sockfd < 0) 
+        fprintf(stderr, "Error opening socket.\n");
+
+    // Bind socket to port
+    if (bind(sockfd, (struct sockaddr *)&server, sizeof(server)) < 0) {
+        fprintf(stderr, "Error binding socket.\n");
+        exit(1);
+    }
+
+    fprintf(stderr, "Opened socket and bound to port %d.\n", port);
+
+    // Listen on port
+    listen(sockfd, 50);
+
+    //Setup aio array, time array, completion array
+    int aio_count = 0;
+    int ret, err;
+    
+    struct aiocb aio_list[AIO_LIST_SIZE];
+    struct time_tuple time_list[AIO_LIST_SIZE];
+    
+    int completed[AIO_LIST_SIZE];
+    for(i=0; i<AIO_LIST_SIZE; i++){
+        completed[i] = 0;
+        time_list[i].start.tv_sec = 0;
+        time_list[i].end.tv_sec = 0;
+    }
+    
+    while(1) {
+    
+        /////////////////////////////////STAGE 1: Accept / Connect / Read if possible///////////////////////////////////////////////////
+        
+        clientsockfd = accept(sockfd, (struct sockaddr *) &client, (socklen_t *) &client_len);
+        
+        if(clientsockfd < 0){
+            //no new connections
+        }
+        else{
+            //printf("Setting up new clientSockFd: %d\n", clientsockfd);
+            
+            //Clear buff and struct
+            memset(&buf2, 0, BUF_SIZE);
+            memset(&aiocb, 0, sizeof(struct aiocb));
+            aiocb.aio_fildes = clientsockfd;
+            aiocb.aio_buf = buf2;
+            aiocb.aio_nbytes = BUF_SIZE;
+
+            //update arrays
+            aio_list[aio_count] = aiocb;
+            gettimeofday(&(time_list[aio_count].start), NULL);
+            
+            aio_count++;
+
+            if (aio_count > AIO_LIST_SIZE){
+                printf("Too many connections for AIO_LIST_SIZE!");
+            }
+            
+            //start download
+            if(aio_read(&aio_list[aio_count-1]) != 0) {
+                printf("Error with aio_read\n");
+            }
+            
+        }
+
+        /////////////////////////////STAGE 2: Iterate through aiocb's, see if any have completed//////////////////////////////////
+        
+        for(i = 0; i < aio_count; i++){
+            
+            if (completed[i] == 1){
+                //already completed read
+                continue;
+            }
+            
+            aiocbp = &aio_list[i];
+            err = aio_error(aiocbp);
+            
+            if (err != 0 && err != EINPROGRESS) {
+              printf ("Error at aio_error() : %s\n", strerror (err));
+            }
+            
+            else if (err == EINPROGRESS){
+                //in progress, don't check return
+            }
+            
+            else{
+                //no error, aio_read is finished
+                ret = aio_return(aiocbp);
+                
+                if (ret < 0) {
+                  printf("Error at aio_return() : %s\n", strerror(errno));
+                }
+            
+                else if (ret > 0){
+                    //Partially finished; read next chunk into buffer
+                    //printf("...Still reading, retval = %d\n", ret);
+                    aio_read(aiocbp);
+                }
+
+                else{
+                    //Read totally finished
+                    //printf ("SUCCESS on %d\n", i);
+                                        
+                    //cleanup:
+                    close(aiocbp->aio_fildes);                    
+                    completed[i] = 1;
+                    
+                    //timing:
+                    gettimeofday(&(time_list[i].end), NULL);
+                    total_finished++;
+                    
+                    suseconds_t startDouble = (time_list[i].start.tv_sec * 1000000 + time_list[i].start.tv_usec);
+                    suseconds_t endDouble = (time_list[i].end.tv_sec * 1000000 + time_list[i].end.tv_usec);
+                    suseconds_t timeDiff = endDouble - startDouble;
+                    
+                    total_time += timeDiff;
+                    
+                    printf("Client #%d took %ldusec to finish. New avg. time: %ldusec\n", i, timeDiff, total_time/total_finished);
+                    
+                }
+            }
         }
     }
     return 0;
@@ -435,13 +593,10 @@ int main(int argc, char **argv) {
        thread_impl();
     }
     else if (strcmp(argv[1], "polling_aio") == 0) {
-
-        will_aio_read = 1;
-        polling_impl();
+        polling_impl_aio();
     }
     else if (strcmp(argv[1], "polling_read") == 0) {
-       will_aio_read = 0;
-       polling_impl();
+        polling_impl_read();
     }
     else {
        select_impl();
